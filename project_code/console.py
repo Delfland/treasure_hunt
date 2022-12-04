37b938276b8210734b37c4f9f836eac2e1d91723
Delfland <d_dallison@hotmail.com>
2022-12-04 00:08:47 +0000

    user repo functions created and tested

--- project_code/console.py
+++ project_code/console.py
@@ -2,9 +2,18 @@ from models.user import User
 
 import repositories.users_repository as users_repository
 
+users_repository.delete_all()
+
 user1 = User("Delphine")
 users_repository.save(user1)
 user2 = User("Eden")
 users_repository.save(user2)
 user3 = User("Mamie")
 users_repository.save(user3)
+
+all_users = users_repository.select_all()
+for user in all_users:
+    print(user.name)
+
+userbyid1 = users_repository.select_by_id(47)
+print(userbyid1.name)
--- project_code/repositories/users_repository.py
+++ project_code/repositories/users_repository.py
@@ -9,6 +9,36 @@ def save(user):
     user.id = results[0]['id']
     return user
 
-# define function delete_all
-# set sql variable equal to delete from users
-# run the sql function on the sql variable
+
+def delete_all():
+    sql = "DELETE FROM users"
+    run_sql(sql)
+
+
+def select_all():
+    users = []
+    sql = "SELECT * FROM users"
+    results = run_sql(sql)
+    for row in results:
+        user = User(row["name"], row["id"])
+        users.append(user)
+    return users
+
+#define function select_by_id with id value as parameter
+def select_by_id(id):
+    #set user variable equal to None
+    user = None
+    # set sql variable equal to select all from users table where id = placeholder
+    sql = "SELECT * FROM users WHERE id = %s"
+    #set values variable equal to id dictionary key
+    values = [id]
+    # set results variable equal to run function run_sql with parameters sql and values
+    results = run_sql(sql, values)
+    # if statement on results = trues:
+    if results:
+    #   result variable is the first index position of results variable
+        result = results[0]
+    #   user variable equals instance of user class with result[attributes]
+        user = User(result['name'], result['id'])
+    #return user variable
+    return user
